87e9da7de126ffe8c1fd550d7bb8233379f242f8
priyakwatra <pkwatra96@gmail.com>
2020-09-04 13:37:07 +0530

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { MatCardModule } from '@angular/material/card';
+import { DescriptionComponent } from './description/description.component';
+import { CartComponentComponent } from './cart-component/cart-component.component';
+import { MatTableModule } from '@angular/material/table';
+import { CdkColumnDef } from '@angular/cdk/table';
+
+const appRoutes = [
+  { path: "", component: ProductsComponent },
+  { path: "description/:product", component: DescriptionComponent },
+  { path: "cart", component: CartComponentComponent }]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    DescriptionComponent,
+    CartComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatTableModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [CdkColumnDef],
+  exports: [RouterModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
